b3c3792a60e112cbe57de4109a3546c07469e892
Jeff Strinko <jeff@pkt3.com>
2013-05-22 19:18:06 -0400

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+/*
+ * grunt-md5versions
+ * https://github.com/jstrinko/grunt-md5versions
+ *
+ * Copyright (c) 2013 Jeff Strinko
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    md5versions: {
+      default_options: {
+        options: {
+					key: 'test'
+        },
+        files: {
+          '/tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+					versions_file: '/tmp/versions-custom.json',
+					key: 'test-custom'
+        },
+        files: {
+          '/tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'md5versions', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/md5versions.js
@@ -0,0 +1,63 @@
+/*
+ * grunt-md5versions
+ * https://github.com/jstrinko/grunt-md5versions
+ *
+ * Copyright (c) 2013 Jeff Strinko
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var Crypto = require('crypto'),
+	_ = require('underscore');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('md5versions', 'Building md5 versions', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+    // Iterate over all specified file groups.
+    this.files.forEach(_.bind(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(_.bind(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }, this)).map(_.bind(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }, this)).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+			var versions;
+			try {
+				versions = grunt.file.readJSON(f.dest);
+			}
+			catch(error) {
+				versions = {};
+			}
+			var md5 = Crypto.createHash('md5');
+			md5.update(src);
+			var digest = md5.digest('hex');
+			console.warn(versions[options.key] + " VS " + digest);
+			if (versions[options.key] !== digest) {
+				// Write the destination file.
+				versions[options.key] = digest;
+				grunt.file.write(f.dest, JSON.stringify(versions));
+				grunt.log.writeln("Digest updated for " + options.key + " to " + digest);
+			}
+    }, this));
+  });
+
+};
